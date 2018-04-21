807a9b7b8b2479a53d36a797bc48a8bd45d59ae8
LukaJankovic <lukjan1999@gmail.com>
2018-04-21 22:00:13 +0200

    init

---
+++ src/window.py
@@ -0,0 +1,37 @@
+# window.py
+#
+# Copyright 2018 LukaJankovic
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from gi.repository import Gtk
+from .gi_composites import GtkTemplate
+
+@GtkTemplate(ui='/org/gnome/Cablegram/login.ui')
+class LoginWindow(Gtk.ApplicationWindow):
+    __gtype_name__ = 'LoginWindow'
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.init_template()
+
+#@GtkTemplate(ui='/org/gnome/Cablegram/login.ui')
+#class CablegramWindow(Gtk.ApplicationWindow):
+#    __gtype_name__ = 'CablegramWindow'
+
+#    label = GtkTemplate.Child()
+
+#    def __init__(self, **kwargs):
+#        super().__init__(**kwargs)
+#        self.init_template()
