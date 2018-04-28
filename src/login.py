c7ff1515175641113d3542dcd7fb834eba0d352e
LukaJankovic <lukjan1999@gmail.com>
2018-04-28 16:20:00 +0200

    fixed binds

--- src/login.py
+++ src/login.py
@@ -21,15 +21,28 @@ from .gi_composites import GtkTemplate
 
 from .universe import Universe
 
+from pathlib import Path
+
+import webbrowser
+import configparser
+
 @GtkTemplate(ui='/org/gnome/Cablegram/login.ui')
 class LoginWindow(Gtk.Assistant):
 
     __gtype_name__ = 'LoginWindow'
 
+    api_page = GtkTemplate.Child()
+    phone_page = GtkTemplate.Child()
+    api_id = GtkTemplate.Child()
+    api_hash = GtkTemplate.Child()
+    phone_entry = GtkTemplate.Child()
+    get_api_keys = GtkTemplate.Child()
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.init_template()
 
+        #Apply CSS
         style_provider = Gtk.CssProvider()
         style_provider.load_from_resource("/org/gnome/Cablegram/login.css")
 
@@ -38,3 +51,36 @@ class LoginWindow(Gtk.Assistant):
             style_provider,
             Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
         )
+
+        #Connections
+        def api_changed(bump):
+            if self.api_id.get_text() and self.api_hash.get_text():
+                self.set_page_complete(self.api_page, True)
+
+        def phone_changed(bump):
+            if self.phone_entry.get_text():
+                self.set_page_complete(self.phone_page, True)
+
+        def open_url(bump):
+            webbrowser.open("https://my.telegram.org/apps")
+
+        self.connect("cancel", exit)
+        self.api_id.connect("changed", api_changed)
+        self.api_hash.connect("changed", api_changed)
+        self.phone_entry.connect("changed", phone_changed)
+        self.get_api_keys.connect("clicked", open_url)
+
+        #Apply ini
+        config = configparser.ConfigParser()
+        config.read(str(Path.home())+"/.config/cablegram.ini")
+
+        try:
+            if config.get("pyrogram", "api_id"):
+                self.api_id.set_text(config.get("pyrogram", "api_id"))
+        except configparser.NoSectionError:
+            print("api_id empty")
+        try:
+            if config.get("pyrogram", "api_hash"):
+                self.api_hash.set_text(config.get("pyrogram", "api_hash"))
+        except configparser.NoSectionError:
+            print("api_hash empty")
